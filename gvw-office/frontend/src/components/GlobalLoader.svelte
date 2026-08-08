d46767e0be99a13c9111faf2ab2930312196aec2
Raphael Schuhmann <raphael221@outlook.de>
2026-08-08 19:38:55 +0200

    Resolved pr finding

--- gvw-office/frontend/src/components/GlobalLoader.svelte
+++ gvw-office/frontend/src/components/GlobalLoader.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { isAppReady } from "../stores/appLoading.svelte.js";
+    import {isAppReady} from "../stores/appLoading.svelte.js";
     import WaveLoadingAnimation from "./WaveLoadingAnimation.svelte";
 
     let {
@@ -28,7 +28,9 @@
 
 {#if showLoader}
     <div class="w-dvw h-dvh flex justify-center items-center bg-white z-9999999 absolute top-0 left-0">
-        <WaveLoadingAnimation title={title} subTitle={subTitle} />
+        {#if showLoader}
+            <WaveLoadingAnimation title={title} subTitle={subTitle}/>
+        {/if}
     </div>
 {:else}
     {@render children()}
